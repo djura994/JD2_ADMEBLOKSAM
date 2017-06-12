37c55dac706a146f9f84b9033f83c5c57c899271
rosicnemanja94 <rosicnemanja94@gmail.com>
2017-06-12 13:20:20 +0200

    Comments and rooms conterollers added.

---
+++ BookingApp/BookingApp/Controllers/CommentsController.cs
@@ -0,0 +1,108 @@
+﻿using BookingApp.Models;
+using ModelClasses;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BookingApp.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        private BAContext db = new BAContext();
+
+        // GET: api/Comments
+        public IQueryable<Comment> GetComments()
+        {
+            return db.comments;
+        }
+
+        // GET: api/Comments/5
+        [ResponseType(typeof(Comment))]
+        public IHttpActionResult GetComment(int id)
+        {
+            Comment comment = db.comments.Find(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(comment);
+        }
+
+        // PUT: api/Comments/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutComment(int id, Comment comment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(comment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Comments
+        [ResponseType(typeof(Comment))]
+        public IHttpActionResult PostComment(Comment comment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.comments.Add(comment);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = comment.Id }, comment);
+        }
+
+        // DELETE: api/Comments/5
+        [ResponseType(typeof(Comment))]
+        public IHttpActionResult DeleteComment(int id)
+        {
+            Comment comment = db.comments.Find(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            db.comments.Remove(comment);
+            db.SaveChanges();
+
+            return Ok(comment);
+        }
+        private bool CommentExists(int id)
+        {
+            return db.comments.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ BookingApp/BookingApp/Controllers/RoomReservationsController.cs
@@ -0,0 +1,109 @@
+﻿using BookingApp.Models;
+using ModelClasses;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BookingApp.Controllers
+{
+    public class RoomReservationsController : ApiController
+    {
+        private BAContext db = new BAContext();
+
+        // GET: api/RoomReservations
+        public IQueryable<RoomReservation> GetRoomReservations()
+        {
+            return db.roomReservations;
+        }
+
+        // GET: api/RoomReservations/5
+        [ResponseType(typeof(RoomReservation))]
+        public IHttpActionResult GetRoomReservations(int id)
+        {
+            RoomReservation roomReservations = db.roomReservations.Find(id);
+            if (roomReservations == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(roomReservations);
+        }
+
+        // PUT: api/RoomReservations/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutRoomReservations(int id, RoomReservation roomReservation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != roomReservation.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(roomReservation).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoomReservationsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = roomReservation.Id }, roomReservation);
+        }
+
+        // POST: api/RoomReservations
+        [ResponseType(typeof(RoomReservation))]
+        public IHttpActionResult PostRoomReservations(RoomReservation roomReservations)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.roomReservations.Add(roomReservations);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = roomReservations.Id }, roomReservations);
+        }
+
+        // DELETE: api/RoomReservations/5
+        [ResponseType(typeof(RoomReservation))]
+        public IHttpActionResult DeleteRoomReservations(int id)
+        {
+            RoomReservation roomReservations = db.roomReservations.Find(id);
+            if (roomReservations == null)
+            {
+                return NotFound();
+            }
+
+            db.roomReservations.Remove(roomReservations);
+            db.SaveChanges();
+
+            return Ok(roomReservations);
+        }
+
+        private bool RoomReservationsExists(int id)
+        {
+            return db.roomReservations.Count(e => e.Id == id) > 0;
+        }
+    }
+}
